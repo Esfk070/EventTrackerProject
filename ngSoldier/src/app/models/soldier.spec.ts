ec610bfba00f95e2f85060057903f07f1853f392
Kevin <esfk070@gmail.com>
2024-09-20 15:04:58 -0600

    Create Entity class in VS code

---
+++ ngSoldier/src/app/models/soldier.spec.ts
@@ -0,0 +1,7 @@
+import { Soldier } from './soldier';
+
+describe('Soldier', () => {
+  it('should create an instance', () => {
+    expect(new Soldier()).toBeTruthy();
+  });
+});
---
+++ ngSoldier/src/app/models/soldier.ts
@@ -0,0 +1,43 @@
+import { last } from "rxjs";
+
+export class Soldier {
+  id: number;
+  militaryRank: string;
+  firstName: string;
+  lastName: string;
+  createDate: string | undefind;
+  lastUpdate: string | undefind;
+  imageUrl: string;
+  enabled: boolean;
+  profile: boolean;
+  description: string;
+  dod: number
+
+  constructor(
+    id: number=0,
+    militaryRank: string = '',
+    firstName: string = '',
+    lastName: string = '',
+    createDate: string = '',
+    lastUpdate: string = '',
+    imageUrl: string = '',
+    enabled: boolean = false,
+    profile: boolean = false,
+    description: string = '',
+    dod: number=0,
+  )
+  {
+    this.id=id;
+    this.militaryRank=militaryRank;
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.createDate=createDate
+    this.lastUpdate=lastUpdate;
+    this.imageUrl=imageUrl;
+    this.enabled=enabled;
+    this.profile=profile;
+    this.description=description;
+    this.dod=dod;
+  }
+
+}
